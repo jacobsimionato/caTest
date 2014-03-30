9f6120646bb2f00c6d30f83b050567eb99fc175b
Jacob Simionato <jacob.simionato@gmail.com>
2014-03-30 12:19:11 +1030

    Initial Commit

---
+++ mipsim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mipsim
+//
+//  Created by Jacob Simionato on 30/03/2014.
+//  Copyright (c) 2014 Jacob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
